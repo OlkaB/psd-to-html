fea0bae550c6711d598a9c7a2c56f7549f62eec9
OlkaB <aleksandra.barecka@gmail.com>
2016-12-18 22:19:31 +0100

    filters i portfolio done, lack of changes on span

--- js.js
+++ js.js
@@ -22,3 +22,42 @@ function revealFunc(){
 
 
 // ---------  PORTFOLIO ----------
+var portfolioPics = document.getElementsByClassName('filter');
+for (i=0; i<portfolioPics.length; i++){
+	portfolioPics[i].addEventListener('click', filterFunc);
+}
+
+console.log(portfolioPics);
+
+function filterFunc() {
+	if(this.id === "web"){
+		var elemToStyle = document.getElementsByClassName('printTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'none';
+		}
+		var elemToStyle = document.getElementsByClassName('webTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'initial';
+		}
+	}
+	else if (this.id === "print") {
+		var elemToStyle = document.getElementsByClassName('printTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'initial';
+		}
+		var elemToStyle = document.getElementsByClassName('webTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'none';
+		}
+	}
+	else if (this.id === "all"){
+		var elemToStyle = document.getElementsByClassName('printTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'initial';
+		}
+		var elemToStyle = document.getElementsByClassName('webTag');
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = 'initial';
+		}
+	}
+}
