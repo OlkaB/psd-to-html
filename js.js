0e014c7ba8664402f215615ce4fae6f8b638dc99
OlkaB <aleksandra.barecka@gmail.com>
2016-12-23 10:30:21 +0100

    naprawione cytaty; start ładowania dodatkowych elementów galerii

--- js.js
+++ js.js
@@ -23,72 +23,123 @@ bttnTxt.textContent = "More";
 
 // ---------  PORTFOLIO FILTER ----------
 var portfolioPics = document.getElementsByClassName("filter");
-for (var i=0; i<portfolioPics.length; i++){
-portfolioPics[i].addEventListener("click", filterFunc);
+for (i=0; i<portfolioPics.length; i++){
+portfolioPics[i].addEventListener("click", porftolioFunc);
 }
 
-function filterFunc() {
-if(this.id === "web"){
-var elemToStyle = document.getElementsByClassName("printTag");
-for (var i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "none";
-}
-var elemToStyle = document.getElementsByClassName("webTag");
-for (var i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "initial";
+function linkStyling(arg){
+	console.log("1works");
+	var thisLink = document.getElementById(arg.id);
+	console.log(arg);
+	thisLink.className+=" active";
+	var sibling = document.getElementById('filter').getElementsByTagName('span');
+	for (i=0; i<sibling.length; i++){
+		if (sibling[i].id !== arg.id){
+			sibling[i].className = "filter";
+		}
+	}
 }
 
-}
-else if (this.id === "print") {
+function filterFunc(arg) {
+if(arg.id === "web"){
 var elemToStyle = document.getElementsByClassName("printTag");
-for (var i=0; i<elemToStyle.length; i++){
+for (i=0; i<elemToStyle.length; i++){
+elemToStyle[i].style.display = "none";
+}
+elemToStyle = document.getElementsByClassName("webTag");
+for (i=0; i<elemToStyle.length; i++){
 elemToStyle[i].style.display = "initial";
 }
-var elemToStyle = document.getElementsByClassName("webTag");
-for (var i=0; i<elemToStyle.length; i++){
+}
+
+else if (arg.id === "print") {
+elemToStyle = document.getElementsByClassName("printTag");
+for (i=0; i<elemToStyle.length; i++){
+elemToStyle[i].style.display = "initial";
+}
+elemToStyle = document.getElementsByClassName("webTag");
+for (i=0; i<elemToStyle.length; i++){
 elemToStyle[i].style.display = "none";
 }
 }
-else if (this.id === "all"){
-var elemToStyle = document.getElementsByClassName("printTag");
-for (var i=0; i<elemToStyle.length; i++){
+
+else if (arg.id === "all"){
+elemToStyle = document.getElementsByClassName("printTag");
+for (i=0; i<elemToStyle.length; i++){
 elemToStyle[i].style.display = "initial";
 }
-var elemToStyle = document.getElementsByClassName("webTag");
-for (var i=0; i<elemToStyle.length; i++){
+elemToStyle = document.getElementsByClassName("webTag");
+for (i=0; i<elemToStyle.length; i++){
 elemToStyle[i].style.display = "initial";
 }
 }
 }
+
+
+function porftolioFunc(){
+	filterFunc(this);
+	linkStyling(this);
+}
+
+
+
+
+// ---------  PORTFOLIO MORE ITEMS ----------
+document.getElementById("browseMore").addEventListener("click", browseMoreFunc);
+
+function browseMoreFunc(){
+	var galleryItm = document.getElementById("gallery");
+	var itemToClone = galleryItm.getElementsByTagName('figure')[0];
+	var cloned = itemToClone.cloneNode(true);
+	var iLoop = 6;
+	for (iLoop; iLoop<iLoop+6; iLoop++){
+		galleryItm.appendChild(cloned);
+		console.log(i);
+	}
+}
+
+
+
 
 
 // ---------  CUSTOMERS' OPINION SWITCHER ----------
 var diodes = document.getElementsByClassName("bttn");
-
-for (var i = 0; i < diodes.length; i++) {
+for (i = 0; i < diodes.length; i++) {
 diodes[i].addEventListener("click", diodeChange);
 }
 
 function diodeChange() {
 this.className = this.className == "bttn diode" ? "bttn currentUse" : "bttn diode";
 
-var citeTxt = document.getElementById("textOutput");
-switch (this.id) {
-case "one":
-citeTxt.textContent = "text one";
-break;
-case "two":
-citeTxt.textContent = "text two";
-break;
-case "three":
-citeTxt.textContent = "text three";
-break;
-}
-
-for (var j = 0; j < diodes.length; j++) {
+for (j = 0; j < diodes.length; j++) {
 if (diodes[j].id !== this.id) {
 diodes[j].className = "bttn diode";
 }
+}
+
+var citeTxt = document.getElementById("cite");
+var citeAuthor = document.getElementById("author");
+var customerImg = document.getElementById("customerPic;");
+switch (this.id) {
+case "one":
+citeTxt.textContent = "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.";
+citeAuthor.textContent = "John Doe";
+break;
+
+case "two":
+citeTxt.textContent = "We can never obtain peace in the outer world until we make peace with ourselves";
+citeAuthor.textContent = "Dalai Lama";
+break;
+
+case "three":
+citeTxt.textContent = "Try not to become a man of success, but rather try to become a man of value. ";
+citeAuthor.textContent = "Albert Einstein";
+break;
+
+case "four":
+citeTxt.textContent = "You must be the change you wish to see in the world.";
+citeAuthor.textContent = "Mahatma Gandhi";
+break;
 }
 }
 
