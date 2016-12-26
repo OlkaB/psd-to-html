a50420ca001dd2f8737ba3ffa9a9fd89ebf388af
OlkaB <o.barecka@gmail.com>
2016-12-26 21:44:38 +0100

    enchanced responsiveness, repaired portfolio appending next items

--- js.js
+++ js.js
@@ -2,21 +2,21 @@
 
 var getBttns = document.getElementsByClassName("revealTxtBttn");
 for (i = 0; i < getBttns.length; i++)  {
-getBttns[i].addEventListener("click", revealFunc);
+	getBttns[i].addEventListener("click", revealFunc);
 }
 
 function revealFunc(){
-console.log(document.getElementById(this.id).textContent);
-var pToChange = document.getElementById(this.id).previousSibling.previousSibling;
-var bttnTxt = document.getElementById(this.id);
-if (document.getElementById(this.id).textContent==="More"){
-pToChange.classList.add("revealedTxt");
-bttnTxt.textContent = "Less";
-}
-else {
-pToChange.classList.remove("revealedTxt");
-bttnTxt.textContent = "More";
-}
+	console.log(document.getElementById(this.id).textContent);
+	var pToChange = document.getElementById(this.id).previousSibling.previousSibling;
+	var bttnTxt = document.getElementById(this.id);
+	if (document.getElementById(this.id).textContent==="More"){
+		pToChange.classList.add("revealedTxt");
+		bttnTxt.textContent = "Less";
+	}
+	else {
+		pToChange.classList.remove("revealedTxt");
+		bttnTxt.textContent = "More";
+	}
 }
 
 
@@ -24,7 +24,7 @@ bttnTxt.textContent = "More";
 // ---------  PORTFOLIO FILTER ----------
 var portfolioPics = document.getElementsByClassName("filter");
 for (i=0; i<portfolioPics.length; i++){
-portfolioPics[i].addEventListener("click", porftolioFunc);
+	portfolioPics[i].addEventListener("click", porftolioFunc);
 }
 
 function linkStyling(arg){
@@ -41,38 +41,38 @@ function linkStyling(arg){
 }
 
 function filterFunc(arg) {
-if(arg.id === "web"){
-var elemToStyle = document.getElementsByClassName("printTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "none";
-}
-elemToStyle = document.getElementsByClassName("webTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "initial";
-}
-}
+	if(arg.id === "web"){
+		var elemToStyle = document.getElementsByClassName("printTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "none";
+		}
+		elemToStyle = document.getElementsByClassName("webTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "initial";
+		}
+	}
 
-else if (arg.id === "print") {
-elemToStyle = document.getElementsByClassName("printTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "initial";
-}
-elemToStyle = document.getElementsByClassName("webTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "none";
-}
-}
+	else if (arg.id === "print") {
+		elemToStyle = document.getElementsByClassName("printTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "initial";
+		}
+		elemToStyle = document.getElementsByClassName("webTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "none";
+		}
+	}
 
-else if (arg.id === "all"){
-elemToStyle = document.getElementsByClassName("printTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "initial";
-}
-elemToStyle = document.getElementsByClassName("webTag");
-for (i=0; i<elemToStyle.length; i++){
-elemToStyle[i].style.display = "initial";
-}
-}
+	else if (arg.id === "all"){
+		elemToStyle = document.getElementsByClassName("printTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "initial";
+		}
+		elemToStyle = document.getElementsByClassName("webTag");
+		for (i=0; i<elemToStyle.length; i++){
+			elemToStyle[i].style.display = "initial";
+		}
+	}
 }
 
 
@@ -104,42 +104,47 @@ function browseMoreFunc(){
 // ---------  CUSTOMERS' OPINION SWITCHER ----------
 var diodes = document.getElementsByClassName("bttn");
 for (i = 0; i < diodes.length; i++) {
-diodes[i].addEventListener("click", diodeChange);
+	diodes[i].addEventListener("click", diodeChange);
 }
 
 function diodeChange() {
-this.className = this.className == "bttn diode" ? "bttn currentUse" : "bttn diode";
+	this.className = this.className == "bttn diode" ? "bttn currentUse" : "bttn diode";
 
-for (j = 0; j < diodes.length; j++) {
-if (diodes[j].id !== this.id) {
-diodes[j].className = "bttn diode";
-}
-}
+	for (j = 0; j < diodes.length; j++) {
+		if (diodes[j].id !== this.id) {
+			diodes[j].className = "bttn diode";
+		}
+	}
 
-var citeTxt = document.getElementById("cite");
-var citeAuthor = document.getElementById("author");
-var customerImg = document.getElementById("customerPic;");
-switch (this.id) {
-case "one":
-citeTxt.textContent = "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.";
-citeAuthor.textContent = "John Doe";
-break;
+	var citeTxt = document.getElementById("cite");
+	var citeAuthor = document.getElementById("author");
+	var customerImg = document.getElementById("customerPic");
+	console.log(customerImg);
+	switch (this.id) {
+		case "one":
+		citeTxt.textContent = "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.";
+		citeAuthor.textContent = "John Doe";
+		customerImg.src = "http://photo.elsoar.com/wp-content/images/Handsome-young-businessman-black-suit.jpg";
+		break;
 
-case "two":
-citeTxt.textContent = "We can never obtain peace in the outer world until we make peace with ourselves";
-citeAuthor.textContent = "Dalai Lama";
-break;
+		case "two":
+		citeTxt.textContent = "We can never obtain peace in the outer world until we make peace with ourselves";
+		citeAuthor.textContent = "Dalai Lama";
+		customerImg.src = "http://placehold.it/250x250";
+		break;
 
-case "three":
-citeTxt.textContent = "Try not to become a man of success, but rather try to become a man of value. ";
-citeAuthor.textContent = "Albert Einstein";
-break;
+		case "three":
+		citeTxt.textContent = "Try not to become a man of success, but rather try to become a man of value. ";
+		citeAuthor.textContent = "Albert Einstein";
+		customerImg.src = "http://placehold.it/350x350";
+		break;
 
-case "four":
-citeTxt.textContent = "You must be the change you wish to see in the world.";
-citeAuthor.textContent = "Mahatma Gandhi";
-break;
-}
+		case "four":
+		citeTxt.textContent = "You must be the change you wish to see in the world.";
+		citeAuthor.textContent = "Mahatma Gandhi";
+		customerImg.src = "http://placehold.it/450x450";
+		break;
+	}
 }
 
 
@@ -147,8 +152,8 @@ break;
 window.addEventListener("load", myMap);
 
 function myMap() {
-var myCenter = new google.maps.LatLng(-8.249410364624302,112.76815445);
-var mapCanvas = document.getElementById("map");
+	var myCenter = new google.maps.LatLng(-8.249410364624302,112.76815445);
+	var mapCanvas = document.getElementById("map");
 //var isDraggable = $(document).width() > 480 ? true : false; // If document (your website) is wider than 480px, isDraggable = true, else isDraggable = false
 var mapOptions = {center: myCenter, zoom: 12, draggable: false, scrollwheel: false};
 var map = new google.maps.Map(mapCanvas, mapOptions);
